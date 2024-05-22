fc00d342cca1f858c0aa5e1ac43e73b28ef168e1
Korugan32 <tahacansenel@gmail.com>
2024-05-22 13:35:21 +0300

    Add Main

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import org.json.simple.parser.ParseException;
+import java.io.IOException;
+
+
+public class main {
+    public static void main(String[] args) throws IOException, ParseException {
+        NewJFrame app = new NewJFrame();
+        app.run();
+    }
+}
+
+
+

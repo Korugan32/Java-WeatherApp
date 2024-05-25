1be897ab0cbeebc14f1451b60b4e2aedbc538b01
Taha Buğra <144429921+tahabugracck@users.noreply.github.com>
2024-05-25 09:52:32 +0300

    Update ApiConnection.java
        'ConnectionUrl' metodunun adını 'connect' olarak değiştirdim.
        'BufferedReader' için try-with-resources yapısını kullandım, böylece otomatik olarak kapanmasını sağladım.
        'response' değişkenini sıfırlamak için herhangi bir işlem eklememe gerek kalmadı çünkü sınıf her çağrıldığında yeni bir instance yaratılıyor.

--- src/ApiConnection.java
+++ src/ApiConnection.java
@@ -1,6 +1,7 @@
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,26 +14,26 @@ public class ApiConnection {
     private JSONParser parser = new JSONParser();
     private JSONObject result;
 
-    String key = "api_key";
+    private final String key = "api_key";
 
-    public void ConnectionUrl(String location) throws IOException, ParseException {
-        URL url = new URL("https://api.weatherapi.com/v1/current.json?key="+key+"&q=" + location + "&aqi=no");
+    public void connect(String location) throws IOException, ParseException {
+        URL url = new URL("https://api.weatherapi.com/v1/current.json?key=" + key + "&q=" + location + "&aqi=no");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
         }
-        in.close();
 
         result = (JSONObject) parser.parse(response.toString());
     }
 
     public String getTemp() {
         JSONObject current = (JSONObject) result.get("current");
-        double temp = (double) current.get("temp_c");
-        return String.valueOf(temp);
+        return String.valueOf(current.get("temp_c"));
     }
 
     public String getStatus() {
